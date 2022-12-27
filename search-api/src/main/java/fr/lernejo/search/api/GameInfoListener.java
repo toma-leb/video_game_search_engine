01c74fade6a309b954667362c2b45cccaac53689
Thomas LEBLOND <thomas.leblond@epita.fr>
2022-12-27 17:37:48 +0100

    Add queue listener

---
+++ search-api/src/main/java/fr/lernejo/search/api/GameInfoListener.java
@@ -0,0 +1,28 @@
+package fr.lernejo.search.api;
+
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.xcontent.XContentType;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+import static fr.lernejo.search.api.AmqpConfiguration.GAME_INFO_QUEUE;
+
+@Component
+public class GameInfoListener {
+    private final RestHighLevelClient restHighLevelClient;
+
+    public GameInfoListener(RestHighLevelClient restHighLevelClient) {
+        this.restHighLevelClient = restHighLevelClient;
+    }
+
+    @RabbitListener(queues = GAME_INFO_QUEUE)
+    public void onMessage(String message, @Header("game_id") String id) throws IOException {
+        IndexRequest indexRequest = new IndexRequest("games").id(id).source(message, XContentType.JSON);
+        this.restHighLevelClient.index(indexRequest, RequestOptions.DEFAULT);
+    }
+}
